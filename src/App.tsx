1e8557f588a1d2c30ea48eabbc85a136b636f245
sandraclarkdev <sandraclarkdev@gmail.com>
2021-08-31 11:19:24 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import {Router, RouteComponentProps} from "@reach/router";
+import {Home} from './pages/home'
+
+let HomePage = (props: RouteComponentProps) => <Home/>;
+
+function App() {
+    return (
+        <>
+            <Router>
+                <HomePage path={"/"}/>
+            </Router>
+
+        </>
+    );
+}
+
+export default App;
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <h2>I am the home Page</h2>
+    )
+}
+
+export default Home;
